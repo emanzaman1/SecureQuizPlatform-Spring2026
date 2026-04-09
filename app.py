8c7578e419d88108bbc70ba463ea1c79870749df
emanzaman1 <emanzaman2122@gmail.com>
2026-04-09 13:43:34 +0500

    Updated app.py with latest changes

--- app.py
+++ app.py
@@ -16,19 +16,3 @@ def result(quiz_id):
 if __name__ == "__main__":
     app.run(debug=True)
 
-=======
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to SecureQuizPlatform!"
-
-@app.route('/result/<int:id>')
-def result(id):
-    return f"Result page for ID {id}"
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> origin/develop
